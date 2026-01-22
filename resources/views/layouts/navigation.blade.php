3b52f5558686c6c13e31f05ffe0dfe7faafb9237
Renzo Vinci <renzovinci@MacBook-Pro-de-Renzo.local>
2026-01-22 14:14:44 -0300

    fix admin control

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }} {{auth()->user()->email}}
+                        {{ __('Dashboard') }} {{ (int) auth()->user()->isAdmin()}}
                     </x-nav-link>
                     @if (session()->has('currentEvent') || auth()->user()?->isAdmin())
                         <x-nav-link :href="route('assignments.index')" :active="request()->routeIs('assignments.index')">
