000a7ad7cee6866eabce0410eadb9502ab57fd48
Renzo Vinci <renzovinci@MacBook-Pro-de-Renzo.local>
2026-01-22 14:26:08 -0300

    fix admin control

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            Usuarios - {{ session('currentEvent')?->name }}
+            Usuarios - {{ session('currentEventName')?->name }}
         </h2>
     </x-slot>
 
