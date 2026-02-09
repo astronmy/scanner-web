5b35a68fcdf87f5798e3311fda316a981f8a4ee1
Renzo Vinci <renzovinci@MacBook-Pro-de-Renzo.local>
2026-02-08 21:34:08 -0300

    fix scan

--- app/Http/Requests/UpdateEventRequest.php
+++ app/Http/Requests/UpdateEventRequest.php
@@ -15,6 +15,7 @@ class UpdateEventRequest extends FormRequest
     {
         return [
             'name'       => ['required', 'string', 'max:255'],
+            'label'       => ['nullable', 'string', 'max:100'],
             'start_date' => ['required', 'date'],
             'end_date'   => ['required', 'date', 'after_or_equal:start_date'],
         ];
@@ -24,6 +25,7 @@ class UpdateEventRequest extends FormRequest
     {
         return [
             'name'       => 'denominación',
+            'label'       => 'etiqueta',
             'start_date' => 'fecha desde',
             'end_date'   => 'fecha hasta',
         ];
