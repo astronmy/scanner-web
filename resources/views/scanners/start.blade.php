31675361b72233b93e0936a8a8d7e4adc22c866a
Renzo Vinci <renzovinci@MacBook-Pro-de-Renzo.local>
2026-03-21 13:59:05 -0300

    iconos

--- resources/views/scanners/start.blade.php
+++ resources/views/scanners/start.blade.php
@@ -116,7 +116,7 @@
 
                     if (data && data.location && data.name) {
                         let control = data.exists == 1
-                            ? '<div class="mt-2 text-red-600 dark:text-red-400 font-semibold">La persona ya ingreso previamente</div>'
+                            ? '<div class="mt-2 text-red-600 dark:text-red-400 font-semibold">La persona ya ingres? previamente</div>'
                             : '';
                         let message = `<center>${data.name}<br>${label}: <br>${data.location}</center>${control}`;
 
